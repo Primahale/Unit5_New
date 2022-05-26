0233cbf295e29af920f318612c25fabc518e078c
Priyanka mahale <mahalepriyanka82@gmail.com>
2022-05-27 00:05:23 +0530

    auth

---
+++ reducer/auth_context/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import { Navbar } from "./components/Navbar";
+import { User } from "./components/User";
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <User></User>
+    </>
+  );
+}
+
+export default App;
